be889f7438989fa16d00f47763e233793b1d77ed
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-04 18:21:07 +0300

    Create abstract class User and classes Admin and Client

---
+++ src/Model/Admin.java
@@ -0,0 +1,10 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class Admin extends User {
+
+    public void checkTicket(Ticket ticket) {
+       // if (LocalDateTime.now() > ticket.getEventTime())
+    }
+}
