b18cfbf827b376c514ab86a0dd75aa2cc662509b
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-06 20:03:22 +0300

    Override toString() method in Admin and Client classes

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -7,4 +7,10 @@ public class Admin extends User {
     public void checkTicket(Ticket ticket) {
        // if (LocalDateTime.now() > ticket.getEventTime())
     }
+
+    @Override
+    public String toString() {
+        return "Admin: " + "\n" +
+                "ID: " + this.getId();
+    }
 }
--- src/Model/Client.java
+++ src/Model/Client.java
@@ -2,4 +2,10 @@ package Model;
 
 public class Client extends User {
 
+    @Override
+    public String toString() {
+        return "Client: " + "\n" +
+                "ID: " + this.getId();
+    }
+
 }
