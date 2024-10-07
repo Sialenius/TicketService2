feedb403fcb67d34ba71dabc87d9304d6995670d
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-07 22:26:38 +0300

    Make variable id in ID class final

--- src/Model/ID.java
+++ src/Model/ID.java
@@ -3,7 +3,7 @@ package Model;
 import java.util.UUID;
 
 public abstract class ID {
-        UUID id = UUID.randomUUID();
+        private final UUID id = UUID.randomUUID();
 
         public UUID getId() {
             return id;
