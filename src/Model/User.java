65d54365d6d023330b4f4cbe23f442783aef38a2
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-10 18:16:52 +0300

    Replace using ID interface to extending abstract class Entity

--- src/Model/User.java
+++ src/Model/User.java
@@ -2,7 +2,7 @@ package Model;
 
 import View.Printable;
 
-public abstract class User implements ID, Printable {
+public abstract class User extends Entity implements Printable {
 
 
     public abstract void printRole();
