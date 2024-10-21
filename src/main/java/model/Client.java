9659ff0daa4f2ff35b5d1f3ad0a2e0a4c941ac2f
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-21 13:32:40 +0300

    Update the Client's constructor

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,8 +1,18 @@
 package model;
 
 
+import model.enums.UserRole;
+
+import java.time.LocalDate;
+
 public class Client extends User {
     private Ticket ticket;
+
+    public Client(String name, LocalDate date) {
+        setName(name);
+        setCreationDate(date);
+        setRole(UserRole.CLIENT);
+    }
 
     public void buyTicket(Ticket ticket) {
         this.ticket = ticket;
@@ -14,9 +24,11 @@ public class Client extends User {
 
     @Override
     public String toString() {
-        return "Client: " + "\n" +
-                "ID: " + this.getId() + "\n" +
-                ticket.toString();
+        return "Client: " + '\n' +
+                "ID: " + this.getId() + '\n' +
+                "Name: " + this.getName() + '\n' +
+                "Creation date: " + this.getCreationDate() + '\n';// +
+                //ticket.toString();
     }
 
     @Override
