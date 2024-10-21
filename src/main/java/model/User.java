15ee6b618f7cca57a12dc83462348815ba837201
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-21 13:33:56 +0300

    Add fields, getters and setters to the User class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,11 +1,48 @@
 package model;
 
 
+import model.enums.UserRole;
 import view.Printable;
 
-public abstract class User extends Entity implements Printable {
+import java.time.LocalDate;
 
+public abstract class User extends Entity implements Printable {
+    private String name;
+    private LocalDate creationDate;
+    private UserRole role;
+
+    public  String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    protected void setCreationDate(LocalDate date) {
+        this.creationDate = date;
+    }
+
+    public String getRole() {
+        return role.getRole();
+    }
+
+    protected void setRole(UserRole role) {
+        this.role = role;
+    }
 
     public abstract void printRole();
+
+    @Override
+    public String toString() {
+        return "Client: " + '\n' +
+                "ID: " + this.getId() + '\n' +
+                "Name: " + this.getName() + '\n' +
+                "Creation date: " + this.getCreationDate() + '\n';
+    }
 
 }
