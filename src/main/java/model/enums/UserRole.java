9ff2f7de25d1f0bfb7dd9a9b49d818358141951c
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-21 13:30:54 +0300

    Create the enum to describe the user's role

---
+++ src/main/java/model/enums/UserRole.java
@@ -0,0 +1,16 @@
+package model.enums;
+
+public enum UserRole {
+    CLIENT("Client"),
+    ADMIN("Admin");
+
+    public String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+ }
