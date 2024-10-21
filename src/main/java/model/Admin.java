5b62891459f12ef6d755fe1c7ba0c989fa810cad
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-21 13:32:19 +0300

    Update the Admin's constructor

--- src/main/java/model/Admin.java
+++ src/main/java/model/Admin.java
@@ -1,9 +1,18 @@
 package model;
 
+import model.enums.UserRole;
+
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Admin extends User {
+
+    public Admin(String name, LocalDate date) {
+        setName(name);
+        setCreationDate(date);
+        setRole(UserRole.ADMIN);
+    }
 
     public void checkTicket(Ticket ticket) {
        if (Timestamp.valueOf(LocalDateTime.now()).after(ticket.getEventTime())) {
