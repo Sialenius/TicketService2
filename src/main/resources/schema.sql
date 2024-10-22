9ea72fc8061c73cafa50946934dfcab8e3e15bc0
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-22 16:27:13 +0300

    Rename DBs' table 'users' to 'users_info'

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TYPE user_role AS ENUM ('CLIENT', 'ADMIN');
 
 CREATE TYPE ticket_type AS ENUM ('DAY', 'WEEK', 'MONTH', 'YEAR');
 
-CREATE TABLE users
+CREATE TABLE users_info
 (
     id VARCHAR(36) PRIMARY KEY NOT NULL,
     name VARCHAR(15) NOT NULL,
@@ -16,6 +16,6 @@ CREATE TABLE tickets
     user_id VARCHAR(36)
     ticket_type ticket_type NOT NULL,
     creation_date date,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE, NOT NULL
+    FOREIGN KEY (user_id) REFERENCES users_info (id) ON DELETE CASCADE, NOT NULL
 );
 
