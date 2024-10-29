37119c7a237c23bfbfd92b8f219b36161733fbff
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-10-29 15:34:48 +0300

    Refactor DAO classes using Hibernate

--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -1,5 +1,9 @@
 package DAO;
 
+import lombok.NonNull;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Optional;
@@ -7,12 +11,7 @@ import java.util.UUID;
 
 public interface DAO<T> {
 
-    Optional<T> fetchByID(UUID id);
+    public void save(T t);
 
-    ArrayList<T> getAll();
-
-    void save(T t);
-
-    void delete(UUID uuid);
-
+    public void delete(@NonNull String id);
 }
--- src/main/java/DAO/TicketDAO.java
+++ src/main/java/DAO/TicketDAO.java
@@ -1,219 +1,91 @@
 package DAO;
 
-import config.ConfigurationObject;
-import config.ConnectionConfiguration;
+import lombok.NonNull;
 import model.Ticket;
+import model.User;
 import model.enums.TicketType;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import view.Printable;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.List;
 
-public class TicketDAO implements DAO<Ticket>, Printable {
-    private static final String SAVE_TICKET_SQL = "INSERT INTO tickets (id, user_id, ticket_type, creation_date) VALUES (?, ?, ?::ticket_type, ?)";
-    private static final String FETCH_TICKET_BY_ID_SQL = "SELECT * FROM tickets WHERE id = ?";
-    private static final String FETCH_TICKET_BY_ID_AND_USER_SQL = "SELECT * FROM tickets WHERE id = ? AND user_id = ? ";
-    private static final String SELECT_ALL_TICKETS_SQL = "SELECT * FROM tickets";
-    private static final String UPDATE_TICKET_TYPE_SQL =  "UPDATE tickets SET ticket_type = ?::ticket_type WHERE id = ?";
-    private static final String DELETE_TICKET_BY_ID_SQL = "DELETE FROM tickets WHERE id = ?";
 
-    private Connection connection;
-
-    private ArrayList<Ticket> tickets = new ArrayList<>();
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
-
-    ConfigurationObject configuration;
-
-    public TicketDAO(ConfigurationObject configuration) {
-        this.configuration = configuration;
-    }
-
-    @Override
+public class TicketDAO implements DAO<Ticket>, Printable  {
     public void save(Ticket ticket) {
-        try {
-            connection = ConnectionConfiguration.getConnection(configuration);
-            preparedStatement = connection.prepareStatement(SAVE_TICKET_SQL);
-            preparedStatement.setString(1, ticket.getID().toString());
-            preparedStatement.setString(2, ticket.getUserID().toString());
-            preparedStatement.setString(3, ticket.getTicketType().name());
-            preparedStatement.setDate(4, Date.valueOf(ticket.getCreationDate()));
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
-            try {
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(ticket);
+        transaction.commit();
+        session.close();
+    }
 
+    public List<Ticket> fetchByTicketAndUserID(String ticketID, User user) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "FROM Ticket t WHERE t.id = :ticket_id AND t.user = :user_id";
+        List ticket = session.createQuery(hql, Ticket.class)
+                .setParameter("ticket_id", ticketID)
+                .setParameter("user_id", user)
+                .getResultList();
+
+        transaction.commit();
+        session.close();
+
+        return ticket;
+    }
+
+    public void updateType(String ticketID, TicketType newType) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "UPDATE Ticket t SET t.ticketType = :newType WHERE t.id = :ticketID";
+        if (session.createQuery(hql)
+                .setParameter("newType", newType)
+                .setParameter("ticketID", ticketID)
+                .executeUpdate() > 0) {
+            System.out.println("Ticket with ID: '" + ticketID + "' was updated.");
         }
-        tickets.add(ticket);
+        else {
+            System.out.println("Ticket with ID: '" + ticketID + "' was not found.");
+        }
+
+        transaction.commit();
+        session.close();
     }
 
     @Override
-    public Optional<Ticket> fetchByID(UUID id) {
-        try {
-            connection = ConnectionConfiguration.getConnection(configuration);
-            preparedStatement = connection.prepareStatement(FETCH_TICKET_BY_ID_SQL);
-            preparedStatement.setString(1, id.toString());
-            resultSet = preparedStatement.executeQuery();
+    public void delete(@NonNull String ticketID) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
 
-            while (resultSet.next()) {
-                UUID columnTicketID = UUID.fromString(resultSet.getString("id"));
-                UUID columnUserID = UUID.fromString(resultSet.getString("user_id"));
-                TicketType columnTicketType =  TicketType.valueOf(resultSet.getString("ticket_type"));
-                LocalDate columnCreationDate = resultSet.getDate("creation_date").toLocalDate();
-
-                return Optional.of(new Ticket(columnTicketID, columnUserID, columnTicketType, columnCreationDate));
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        Ticket ticket = session.get(Ticket.class, ticketID);
+        if (ticket != null) {
+            session.delete(ticket);
+            System.out.println("Ticket with ID: '" + ticketID + "' was deleted.");
+        } else {
+            System.out.println("Ticket with ID: '" + ticketID + "' was not found.");
         }
-        finally {
-            try {
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return Optional.empty();
-    }
 
-    public Optional<Ticket> fetchByTicketAndUserID(UUID ticketID, UUID userID) {
-        try {
-            connection = ConnectionConfiguration.getConnection(configuration);
-            preparedStatement = connection.prepareStatement(FETCH_TICKET_BY_ID_AND_USER_SQL);
-            preparedStatement.setString(1, ticketID.toString());
-            preparedStatement.setString(2, userID.toString());
-            resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                UUID columnTicketID = UUID.fromString(resultSet.getString("id"));
-                UUID columnUserID = UUID.fromString(resultSet.getString("user_id"));
-                TicketType columnTicketType =  TicketType.valueOf(resultSet.getString("ticket_type"));
-                LocalDate columnCreationDate = resultSet.getDate("creation_date").toLocalDate();
-
-                return Optional.of(new Ticket(columnTicketID, columnUserID, columnTicketType, columnCreationDate));
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e);
-            throw new RuntimeException(e);
-        }
-        finally {
-            try {
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        System.out.println("Invalid ticket or user UUID");
-        return Optional.empty();
-    }
-
-    @Override
-    public ArrayList<Ticket> getAll() {
-        tickets.clear();
-
-        try {
-            connection = ConnectionConfiguration.getConnection(configuration);
-            preparedStatement = connection.prepareStatement(SELECT_ALL_TICKETS_SQL);
-            resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-
-                UUID columnID = UUID.fromString(resultSet.getString("id"));
-                UUID columnUserID = UUID.fromString(resultSet.getString("user_id"));
-                TicketType columnTicketType = TicketType.valueOf(resultSet.getString("ticket_type"));
-                LocalDate columnCreationDate = resultSet.getDate("creation_date").toLocalDate();
-
-                tickets.add(new Ticket(columnID, columnUserID, columnTicketType, columnCreationDate));
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        finally {
-            try {
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return tickets;
-    }
-
-    public void update(UUID id, TicketType newType) {
-        try {
-            connection = ConnectionConfiguration.getConnection(configuration);
-            preparedStatement = connection.prepareStatement(UPDATE_TICKET_TYPE_SQL);
-            preparedStatement.setString(1, newType.name());
-            preparedStatement.setString(2, id.toString());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        finally {
-            try {
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    @Override
-    public void delete(UUID uuid) {
-        try {
-            connection = ConnectionConfiguration.getConnection(configuration);
-            preparedStatement = connection.prepareStatement(DELETE_TICKET_BY_ID_SQL);
-            preparedStatement.setString(1, uuid.toString());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
-            try {
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void printInformation() {
-        tickets = this.getAll();
-        System.out.println("=============================================== TABLE 'TICKETS' ===========================================\n" +
-                "                 id                  |                user_id               |  ticket_type  | creation_date\n" +
-                "-----------------------------------------------------------------------------------------------------------");
-        if (tickets.isEmpty()) {
-            System.out.println("empty\n");
-        } else {
-            for (Ticket t : tickets) {
-                System.out.println(t.getID() + " | " + t.getUserID() + " |      " + t.getTicketType() + "     | " + t.getCreationDate());
-            }
-            System.out.print('\n');
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "FROM Ticket";
+        List<Ticket> tickets = session.createQuery(hql, Ticket.class).getResultList();
+
+        for (Ticket t: tickets) {
+            System.out.println(t.getId() + " | " + t.getTicketType() + " | " + t.getUser().getId());
         }
+
+        transaction.commit();
+        session.close();
     }
+
 }
