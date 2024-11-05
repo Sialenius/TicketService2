a49a14b4344fdbec5a4910b578f6fb5fac2b107c
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-11-05 10:47:06 +0300

    Update UserDAO using JDBC

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -1,206 +1,72 @@
 package DAO;
 
-import config.ConfigurationObject;
-import config.ConnectionConfiguration;
-import model.Admin;
-import model.Client;
 import model.User;
-import model.enums.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import view.Printable;
 
 import javax.sql.DataSource;
 import java.sql.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 
 @Repository
-public class UserDAO implements DAO<User>, Printable {
+public class UserDAO implements DAO<User> {
     private static final String DELETE_ALL_USERS_SQL = "DELETE FROM users_info";
     private static final String SAVE_USER_SQL = "INSERT INTO users_info (id, name, creation_date, user_role) VALUES (?, ?, ?, ?::user_role)";
     private static final String FETCH_USER_BY_ID_SQL = "SELECT * FROM users_info WHERE id = ?";
+    private static final String FETCH_USER_BY_NAME_SQL = "SELECT * FROM users_info WHERE name = ?";
     private static final String FETCH_ALL_USERS_SQL = "SELECT * FROM users_info";
     private static final String DELETE_USER_BY_ID_SQL = "DELETE FROM users_info WHERE id = ?";
-
-    private Connection connection;
-    
-    private ArrayList<User> users = new ArrayList<>();
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
-
-    private ConfigurationObject configuration;
+    private static final String UPDATE_USER_ID = "UPDATE users_info SET id = ? WHERE id = ?";
 
     private DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
-    public UserDAO(ConfigurationObject configuration) {
-        this.configuration = configuration;
-    }
-
-    public UserDAO(@Autowired DataSource dataSource) {
+    @Autowired
+    public UserDAO(DataSource dataSource, JdbcTemplate jdbcTemplate) {
         this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void deleteAll() {
-        try {
-            connection = dataSource.getConnection();
-            System.out.println(connection);
-            preparedStatement = connection.prepareStatement(DELETE_ALL_USERS_SQL);
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.println(e);
-            throw new RuntimeException(e);
-
-        }
-        finally {
-            try {
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    public List<User> getAll() {
+        return jdbcTemplate.query(FETCH_ALL_USERS_SQL, new UserMapper());
     }
 
     @Override
     public void save(User user) {
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(SAVE_USER_SQL);
-            preparedStatement.setString(1, user.getID().toString());
-            preparedStatement.setString(2, user.getName());
-            preparedStatement.setTimestamp(3, user.getCreationDate());
-            preparedStatement.setString(4, user.getRole().name());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
-            try {
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        jdbcTemplate.update(SAVE_USER_SQL, user.getId(), user.getName(), user.getCreationDate(), user.getRole().name());
+        System.out.println(user.getRole().name());
+    }
+
+    public void deleteAll() {
+        jdbcTemplate.execute(DELETE_ALL_USERS_SQL);
+    }
+
+    public void updateID(UUID userID, UUID newID) {
+        jdbcTemplate.update(UPDATE_USER_ID, newID.toString(), userID.toString());
     }
 
     @Override
-    public Optional<User> fetchByID(UUID id)  {
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(FETCH_USER_BY_ID_SQL);
-            preparedStatement.setString(1, id.toString());
-            resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                UUID columnID = UUID.fromString(resultSet.getString("id"));
-                String columnName = resultSet.getString("name");
-                Timestamp columnCreationDate = resultSet.getTimestamp("creation_date");
-
-                switch (resultSet.getString("user_role")) {
-                    case "CLIENT" :
-                        return Optional.of(new Client(columnID, columnName, columnCreationDate));
-                    case "ADMIN" :
-                        return Optional.of(new Admin(columnID, columnName, columnCreationDate));
-                }
-
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
-            try {
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return Optional.empty();
+    public User fetchByID(UUID id)  {
+        return jdbcTemplate.query(FETCH_USER_BY_ID_SQL, new Object[]{id.toString()}, new UserMapper())
+                .stream().findFirst().orElseThrow();
     }
 
-    @Override
-    public ArrayList<User> getAll() {
-        users.clear();
-
-        try {
-            connection = dataSource.getConnection();
-
-            preparedStatement = connection.prepareStatement(FETCH_ALL_USERS_SQL);
-            resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-
-                UUID columnID = UUID.fromString(resultSet.getString("id"));
-                String columnName = resultSet.getString("name");
-                Timestamp columnCreationDate = resultSet.getTimestamp("creation_date");
-                UserRole columnRole = UserRole.valueOf(resultSet.getString("user_role"));
-
-                switch (columnRole) {
-                    case CLIENT -> users.add(new Client(columnID, columnName, columnCreationDate));
-                    case ADMIN -> users.add(new Admin(columnID, columnName, columnCreationDate));
-                }
-            }
-
-        } catch (SQLException e) {
-           // System.out.println(e);
-            throw new RuntimeException(e);
-        }
-        finally {
-            try {
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return users;
+    public User fetchByName(String name)  {
+        return jdbcTemplate.query(FETCH_USER_BY_NAME_SQL, new Object[]{name}, new UserMapper())
+                .stream().findFirst().orElseThrow();
     }
-
 
     @Override
     public void delete(UUID userID) {
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(DELETE_USER_BY_ID_SQL);
-            preparedStatement.setString(1, userID.toString());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
-            try {
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        jdbcTemplate.update(DELETE_USER_BY_ID_SQL, userID.toString());
+
     }
 
-    @Override
-    public void printInformation() {
-        users = this.getAll();
-
-        System.out.println("=========================== TABLE 'USERS_INFO' ==========================\n" +
-                "                  id                 |  name  | creation_date | user_role  \n" +
-                "--------------------------------------------------------------");
-        if (users.isEmpty()) {
-            System.out.println("empty\n");
-        } else {
-            for (User u: users) {
-                System.out.println(u.getID() + " | " + u.getName() + " | " + u.getCreationDate() + " | " + u.getRole());
-            }
-            System.out.print('\n');
-        }
-    }
 }
