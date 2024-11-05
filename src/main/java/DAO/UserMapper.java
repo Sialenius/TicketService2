0c008dff866af0d4644cc2fdd332ab651633bbfb
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-11-05 10:58:40 +0300

    Update UserMapper

--- src/main/java/DAO/UserMapper.java
+++ src/main/java/DAO/UserMapper.java
@@ -1,5 +1,7 @@
 package DAO;
 
+import model.Admin;
+import model.Client;
 import model.User;
 import model.enums.UserRole;
 import org.springframework.jdbc.core.RowMapper;
@@ -12,12 +14,13 @@ public class UserMapper implements RowMapper<User> {
 
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-        User user = new User();
-        user.setId(UUID.fromString(rs.getString("id")));
-        user.setName(rs.getString("name"));
-        user.setCreationDate(rs.getTimestamp("creation_date"));
-        user.setRole(UserRole.valueOf(rs.getString("user_role")));
-
+        User user = null;
+        switch (UserRole.valueOf(rs.getString("user_role"))) {
+            case ADMIN ->
+                    user = new Admin(UUID.fromString(rs.getString("id")), rs.getString("name"), rs.getTimestamp("creation_date"));
+            case CLIENT ->
+                user = new Client(UUID.fromString(rs.getString("id")), rs.getString("name"), rs.getTimestamp("creation_date"));
+        }
         return user;
     }
 }
