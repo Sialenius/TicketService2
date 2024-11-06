c214e632f17e53f7548b31db3d558c8f9471c7fc
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-11-06 09:25:52 +0300

    Make just one service class

--- src/main/java/service/ApplicationService.java
+++ src/main/java/service/ApplicationService.java
@@ -1,8 +1,9 @@
 package service;
 
 import DAO.TicketDAO;
-import model.*;
-
+import DAO.UserDAO;
+import model.Ticket;
+import model.User;
 import model.enums.TicketType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,23 +15,71 @@ import view.Printable;
 import java.util.List;
 import java.util.UUID;
 
-
 @Service
-public class TicketService implements Printable {
+public class UserService implements Printable {
+
+    @Autowired
+    private UserDAO userDAO;
 
     @Autowired
     private TicketDAO ticketDAO;
 
     @Value("${isUsersUpdateAndTicketsCreationAvailable}")
-    private boolean isTicketsCreationAvailable;
+    private boolean isUsersUpadeAvailable;
 
 
-    public List<Ticket> getAll() {
+    public List<User> getAllUsers() {
+        return userDAO.getAll();
+    }
+
+
+    @Transactional
+    public void registerUser(User user) {
+        userDAO.save(user);
+    }
+
+    @Transactional
+    public User fetchByUserID(UUID userID) {
+        User user = userDAO.fetchByID(userID);
+        if (user == null) {
+            System.out.println("The user with ID: '" + userID + "' doesn't exist.");
+            System.exit(0);
+        }
+        return user;
+    }
+
+
+    @Transactional
+    public void updateUserID(UUID userID, UUID newID) throws IllegalAccessException {
+       if (isUsersUpadeAvailable == true) {
+           fetchByUserID(userID);
+
+           userDAO.updateID(userID, newID);
+       } else {
+           throw new IllegalAccessException("Updating users is no available");
+       }
+
+    }
+
+    @Transactional
+    public void removeUser(UUID userID) {
+        fetchByUserID(userID);
+
+        userDAO.delete(userID);
+    }
+
+    @Transactional
+    public void deleteAllUsers() {
+        userDAO.deleteAll();
+    }
+
+
+    public List<Ticket> getAllTickets() {
         return ticketDAO.getAll();
     }
 
     @Transactional
-    public void register(Ticket ticket) {
+    public void registerTicket(Ticket ticket) {
         if (isTicketsCreationAvailable == true) {
             ticketDAO.save(ticket);
         } else {
@@ -39,8 +88,7 @@ public class TicketService implements Printable {
     }
 
 
-
-    public Ticket fetchByID(UUID ticketID) {
+    public Ticket fetchTicketByID(UUID ticketID) {
         Ticket ticket = ticketDAO.fetchByID(ticketID);
         if (ticket == null) {
             System.out.println("The ticket with ID: '" + ticketID + "' doesn't exist.");
@@ -50,7 +98,7 @@ public class TicketService implements Printable {
     }
 
 
-    public Ticket fetchByUserAndTicketID(UUID userID, UUID ticketID) {
+    public Ticket fetchTicketByUserAndTicketID(UUID userID, UUID ticketID) {
         Ticket ticket = ticketDAO.fetchByTicketAndUserID(userID, ticketID);
         if (ticket == null) {
             System.out.println("The ticket with ID: '" + ticketID + "' and userID: '" + userID + "' doesn't exist.");
@@ -61,13 +109,13 @@ public class TicketService implements Printable {
 
     @Transactional
     public void updateTicketType(UUID ticketID, TicketType newTicketType) {
-        fetchByID(ticketID);
+        fetchTicketByID(ticketID);
 
         ticketDAO.update(ticketID, newTicketType);
     }
 
-    @Override
-    public void printInformation() {
+
+    public void printTicketInformation() {
         System.out.println("=============================================== TABLE 'TICKETS' ===========================================\n" +
                 "                 id                  |                user_id               |  ticket_type  | creation_date\n" +
                 "-----------------------------------------------------------------------------------------------------------");
@@ -76,6 +124,21 @@ public class TicketService implements Printable {
         } else {
             for (Ticket t : ticketDAO.getAll()) {
                 System.out.println(t.getId() + " | " + t.getUserID() + " |      " + t.getTicketType() + "     | " + t.getCreationDate());
+            }
+            System.out.print('\n');
+        }
+    }
+
+ 
+    public void printUsersInformation() {
+        System.out.println("=========================== TABLE 'USERS_INFO' ==========================\n" +
+                "                  id                 |  name  | creation_date | user_role  \n" +
+                "--------------------------------------------------------------");
+        if (userDAO.getAll().isEmpty()) {
+            System.out.println("empty\n");
+        } else {
+            for (User u: userDAO.getAll()) {
+                System.out.println(u.getId() + " | " + u.getName() + " | " + u.getCreationDate() + " | " + u.getRole());
             }
             System.out.print('\n');
         }
