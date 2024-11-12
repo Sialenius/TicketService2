612735fee214ef90c3f6d9248bf0959caa0bec1f
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-11-12 14:31:48 +0300

    Use CRUD repository instead of JPA

--- src/main/java/com/project/jfb/repository/TicketRepository.java
+++ src/main/java/com/project/jfb/repository/TicketRepository.java
@@ -2,10 +2,11 @@ package com.project.jfb.repository;
 
 import com.project.jfb.io.entity.TicketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
+public interface TicketRepository extends CrudRepository<TicketEntity, Long> {
 }
--- src/main/java/com/project/jfb/repository/UserRepository.java
+++ src/main/java/com/project/jfb/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.project.jfb.repository;
 
 import com.project.jfb.io.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
 }
