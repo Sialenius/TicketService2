53bf55520fc17af096fb9833859ca7f02eaef0d7
aliaksei.sialenia <aliaksei.sialenia@gmail.com>
2024-11-14 12:38:26 +0300

    Update conditional bean. Create AppConfiguration

--- src/main/java/com/project/jfb/config/ThisIsMyFirstConditionalBean.java
+++ src/main/java/com/project/jfb/config/ThisIsMyFirstConditionalBean.java
@@ -1,17 +1,6 @@
 package com.project.jfb.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
 public class ThisIsMyFirstConditionalBean {
-    
-    @Bean
-    @ConditionalOnProperty(name = "isConditionalBeanAvailable", havingValue = "true")
-    public ThisIsMyFirstConditionalBean thisIsMyFirstConditionalBean() {
-        return new ThisIsMyFirstConditionalBean();
-    }
+
 }
